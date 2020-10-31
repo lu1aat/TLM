4f544f1fcdc3b970788eda3d50fa380b7bd35896
Andres Tarantini <andres.tarantini@mercadolibre.com>
2020-10-31 18:43:52 -0300

    Script csv2kml: Show report stations in map with line.

--- tlm-csv2kml.py
+++ tlm-csv2kml.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     path_points = []
     point_id = 0
     one_grid_only = []
+    reporters = {}
     for line in lines[1:]:
             fields = line.strip().split(",")
             print(fields)
@@ -31,6 +32,17 @@ if __name__ == '__main__':
             pnt.style.iconstyle.icon.href = 'http://earth.google.com/images/kml-icons/track-directional/track-none.png'
             point_id = point_id + 1
             one_grid_only.append(fields[6])
+
+            # reporters
+            reporters[fields[12]] = (fields[14], fields[13])
+            lin_rpt = kml.newlinestring(name=fields[12], description="", coords=[(fields[14], fields[13]), (fields[8], fields[7])])
+
+    # Reporters
+    for rpt_call, rpt_data in reporters.items():
+        print(rpt_call, rpt_data)
+        pnt = kml.newpoint(name=rpt_call, coords=[rpt_data])
+        pnt.style.iconstyle.scale = 3  # Icon thrice as big
+        pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/paddle/grn-blank.png'
 
     lin = kml.newlinestring(name="LU1ESY", description="LU1ESY Flight Path", coords=path_points)
     lin.style.linestyle.color = simplekml.Color.red  # Red
